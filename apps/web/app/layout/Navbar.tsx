31bf4e7329ced7861c79b87a0e8dfde164bf3d54
Josh Smith <79778337+jshsmth@users.noreply.github.com>
2025-03-01 15:11:44 +1100

    chore: split up cart components

--- apps/web/app/layout/Navbar.tsx
+++ apps/web/app/layout/Navbar.tsx
@@ -3,7 +3,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import Cart from "./Cart";
+import { Cart } from "../_cart/Cart";
 
 interface NavbarProps {
   username?: string;
